838f7adbf7ce0531903c8b80ec30ed0a0d585875
oanh <oanhdoanthi5@gmail.com>
2025-03-31 22:56:25 +0700

    Thêm Form để thêm liên hệ mới

---
+++ ContactList/AddContactForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ContactList
+{
+    partial class AddContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_phonenumber = new System.Windows.Forms.TextBox();
+            this.bt_savecontact = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ Tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số điện thoại";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(106, 27);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(133, 22);
+            this.txt_name.TabIndex = 2;
+            // 
+            // txt_phonenumber
+            // 
+            this.txt_phonenumber.Location = new System.Drawing.Point(106, 68);
+            this.txt_phonenumber.Name = "txt_phonenumber";
+            this.txt_phonenumber.Size = new System.Drawing.Size(133, 22);
+            this.txt_phonenumber.TabIndex = 3;
+            // 
+            // bt_savecontact
+            // 
+            this.bt_savecontact.Location = new System.Drawing.Point(106, 107);
+            this.bt_savecontact.Name = "bt_savecontact";
+            this.bt_savecontact.Size = new System.Drawing.Size(133, 23);
+            this.bt_savecontact.TabIndex = 4;
+            this.bt_savecontact.Text = "Lưu";
+            this.bt_savecontact.UseVisualStyleBackColor = true;
+            // 
+            // AddContactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_savecontact);
+            this.Controls.Add(this.txt_phonenumber);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddContactForm";
+            this.Text = "AddContactForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_phonenumber;
+        private System.Windows.Forms.Button bt_savecontact;
+    }
+}
--- ContactList/MainForm.Designer.cs
+++ ContactList/MainForm.Designer.cs
@@ -30,33 +30,59 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(253, 55);
+            this.label1.Location = new System.Drawing.Point(337, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.Size = new System.Drawing.Size(122, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Danh Sách Liên Hệ";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(410, 50);
+            this.button1.Location = new System.Drawing.Point(547, 62);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.Size = new System.Drawing.Size(149, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Thêm Liên Hệ";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(547, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Thêm";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Thêm liên hệ";
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -68,6 +94,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
